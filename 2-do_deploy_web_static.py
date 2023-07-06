c5e62f788a82211ca427abe8a6041ac9449907b8
Ivy <ivystacyn@gmail.com>
2023-07-06 11:04:12 +0000

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,25 +18,28 @@ def do_deploy(archive_path):
         # Upload archive to /tmp/ directory of web server
         put(archive_path, '/tmp/')
 
-        # Extract the archive to /data/web_static/releases/<archive filename without extension>/
+        # Uncompress archive to folder on the web server
         archive_filename = archive_path.split('/')[-1]
         folder_name = archive_filename.split('.')[0]
         releases_path = '/data/web_static/releases/'
         run('mkdir -p {}{}/'.format(releases_path, folder_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_filename, releases_path, folder_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_filename,
+            releases_path, folder_name))
 
         # Delete the archive from the web server
         run('rm /tmp/{}'.format(archive_filename))
 
         # Move extracted files to correct location
-        run('mv {}{}/web_static/* {}{}/'.format(releases_path, folder_name, releases_path, folder_name))
+        run('mv {}{}/web_static/* {}{}/'.format(releases_path,
+            folder_name, releases_path, folder_name))
         run('rm -rf {}{}/web_static'.format(releases_path, folder_name))
 
         # Delete the existing symbolic link
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link to new version
-        run('ln -s {}{}/ /data/web_static/current'.format(releases_path, folder_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(
+            releases_path, folder_name))
 
         return True
 
