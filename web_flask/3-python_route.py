d54eb3e738e9d6c02413929ce1eda039f5bd8224
Ivy <ivystacyn@gmail.com>
2023-07-22 07:16:58 +0000

    Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,10 +26,11 @@ def c(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Displays “Python ” followed by the value of the text variable """
-    return “Python ” + text.replace('_', ' ')
+    return "Python " + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
